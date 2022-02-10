7d8c96ec250016fd67adc7279980ffe9c1a0520c
Felippe <frafaelrls@gmail.com>
2022-02-10 14:54:04 -0300

    adding main.py com intrudução

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+import os
+
+introducao = "O mistério da Múmia Desaparecida\n\n" \
+         "Você está prestes a iniciar a leitura de um livro " \
+         "interativo.\nSaiba que suas escolhas trarão consequências.\nVocê " \
+         "possui nível suficiente para essa aventura?\n\n"\
+         "Digite Sim para iniciar, digite Não se precisar de mais XP.\n"
+
+for letra in introducao:
+    time.sleep(0.08)
+    print(letra, end='', flush=True)
+
+
+resposta = input()
+resposta = resposta.lower()
+if resposta == "sim":
+    print("Que a ventura comesse.\n")
+    print("INICIA O LIVRO")
+    os.system("pause")
+else:
+    print("Volte quando tiver mais XP\n")
+    print("FECHA O LIVRO")
+    os.system("pause")
