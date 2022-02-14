c03d60df0ed14c83f24682d8d30e47e6e9ba225d
Felippe <frafaelrls@gmail.com>
2022-02-14 15:53:07 -0300

    adding logica.py file

---
+++ logica.py
@@ -0,0 +1,7 @@
+"""
+- Arquivo para a lógica de funcionamento do livro;
+- Deverá conter as chamadas das funções das páginas;
+- Fornecer uma resposta para o usuário conforme suas interações;
+"""
+import paginas
+
