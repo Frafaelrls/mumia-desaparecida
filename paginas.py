445c366cf330da1ed8432eed013ca861f8391c46
Felippe <frafaelrls@gmail.com>
2022-02-14 15:54:20 -0300

    adding paginas.py file

---
+++ paginas.py
@@ -0,0 +1,141 @@
+"""Páginas do livro"""
+
+
+def pagina_01():
+    print(f"Texto da página 01")
+    with open(r'txt\pagina 01.txt', 'r', encoding='utf-8') as pagina_01X:
+        pagina = pagina_01X.read()
+        print(pagina)
+    return
+
+
+def pagina_02():
+    print(f"Texto da página 02")
+    with open(r'txt\pagina 02.txt', 'r', encoding='utf-8') as pagina_02X:
+        pagina = pagina_02X.read()
+        print(pagina)
+    return
+
+
+def pagina_03():
+    print(f"Texto da página 03")
+    return
+
+
+def pagina_04():
+    print(f"Texto da página 04")
+    return
+
+
+def pagina_05():
+    print(f"Texto da página 05")
+    return
+
+
+def pagina_06():
+    print(f"Texto da página 06")
+    return
+
+
+def pagina_07():
+    print(f"Texto da página 07")
+    return
+
+
+def pagina_08():
+    print(f"Texto da página 08")
+    return
+
+
+def pagina_09():
+    print(f"Texto da página 09")
+    return
+
+
+def pagina_10():
+    print(f"Texto da página 10")
+    return
+
+
+def pagina_11():
+    print(f"Texto da página 11")
+    return
+
+
+def pagina_12():
+    print(f"Texto da página 12")
+    return
+
+
+def pagina_13():
+    print(f"Texto da página 13")
+    return
+
+
+def pagina_14():
+    print(f"Texto da página 15")
+    return
+
+
+def pagina_16():
+    print(f"Texto da página 16")
+    return
+
+
+def pagina_17():
+    print(f"Texto da página 18")
+    return
+
+
+def pagina_19():
+    print(f"Texto da página 19")
+    return
+
+
+def pagina_20():
+    print(f"Texto da página 02")
+    return
+
+
+def pagina_21():
+    print(f"Texto da página 21")
+    return
+
+
+def pagina_22():
+    print(f"Texto da página 22")
+    return
+
+
+def pagina_23():
+    print(f"Texto da página 23")
+    return
+
+
+def pagina_24():
+    print(f"Texto da página 24")
+    return
+
+
+def pagina_25():
+    print(f"Texto da página 25")
+    return
+
+
+def pagina_26():
+    print(f"Texto da página 26")
+    return
+
+
+def pagina_27():
+    print(f"Texto da página 27")
+    return
+
+
+def pagina_28():
+    print(f"Texto da página 28")
+    return
+
+
+pagina_01()
+pagina_02()
