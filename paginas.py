7b7213f9922c871b599ec3ea6e0551ae9136f9d1
Felippe <frafaelrls@gmail.com>
2022-02-14 17:27:53 -0300

    adding letter by letter on pagina_01 fuction

--- paginas.py
+++ paginas.py
@@ -1,11 +1,16 @@
 """Páginas do livro"""
+import time
+import logica
 
 
 def pagina_01():
+    logica.apagar()
     print(f"Texto da página 01")
     with open(r'txt\pagina 01.txt', 'r', encoding='utf-8') as pagina_01X:
         pagina = pagina_01X.read()
-        print(pagina)
+        for palavra in pagina:
+            time.sleep(0.08)
+            print(palavra, end='', flush=True)
     return
 
 
@@ -201,8 +206,201 @@ def pagina_30():
     return
 
 
+def pagina_31():
+    print(f'Texto da página 31')
+    with open(r'txt\pagina 31.txt', 'r', encoding='utf-8') as pagina_31X:
+        pagina = pagina_31X.read()
+        print(pagina)
+    return
+
+
+def pagina_32():
+    print(f'Texto da página 32')
+    with open(r'txt\pagina 32.txt', 'r', encoding='utf-8') as pagina_32X:
+        pagina = pagina_32X.read()
+        print(pagina)
+    return
+
+
+def pagina_33():
+    print(f'Texto da página 33')
+    with open(r'txt\pagina 33.txt', 'r', encoding='utf-8') as pagina_33X:
+        pagina = pagina_33X.read()
+        print(pagina)
+    return
+
+
+def pagina_34():
+    print(f'Texto da página 34')
+    with open(r'txt\pagina 34.txt', 'r', encoding='utf-8') as pagina_34X:
+        pagina = pagina_34X.read()
+        print(pagina)
+    return
+
+
+def pagina_35():
+    print(f'Texto da página 35')
+    with open(r'txt\pagina 35.txt', 'r', encoding='utf-8') as pagina_35X:
+        pagina = pagina_35X.read()
+        print(pagina)
+    return
+
+
+def pagina_36():
+    print(f'Texto da página 36')
+    with open(r'txt\pagina 36.txt', 'r', encoding='utf-8') as pagina_36X:
+        pagina = pagina_36X.read()
+        print(pagina)
+    return
+
+
+def pagina_37():
+    print(f'Texto da página 37')
+    with open(r'txt\pagina 37.txt', 'r', encoding='utf-8') as pagina_37X:
+        pagina = pagina_37X.read()
+        print(pagina)
+    return
+
+
+def pagina_38():
+    print(f'Texto da página 38')
+    with open(r'txt\pagina 38.txt', 'r', encoding='utf-8') as pagina_38X:
+        pagina = pagina_38X.read()
+        print(pagina)
+    return
+
+
+def pagina_39():
+    print(f'Texto da página 39')
+    with open(r'txt\pagina 39.txt', 'r', encoding='utf-8') as pagina_39X:
+        pagina = pagina_39X.read()
+        print(pagina)
+    return
+
+
+def pagina_40():
+    print(f'Texto da página 40')
+    with open(r'txt\pagina 40.txt', 'r', encoding='utf-8') as pagina_40X:
+        pagina = pagina_40X.read()
+        print(pagina)
+    return
+
+
+def pagina_41():
+    print(f'Texto da página 41')
+    with open(r'txt\pagina 41.txt', 'r', encoding='utf-8') as pagina_41X:
+        pagina = pagina_41X.read()
+        print(pagina)
+    return
+
+
+def pagina_42():
+    print(f'Texto da página 42')
+    with open(r'txt\pagina 42.txt', 'r', encoding='utf-8') as pagina_42X:
+        pagina = pagina_42X.read()
+        print(pagina)
+    return
+
+
+def pagina_43():
+    print(f'Texto da página 43')
+    with open(r'txt\pagina 43.txt', 'r', encoding='utf-8') as pagina_43X:
+        pagina = pagina_43X.read()
+        print(pagina)
+    return
+
+
+def pagina_44():
+    print(f'Texto da página 44')
+    with open(r'txt\pagina 44.txt', 'r', encoding='utf-8') as pagina_44X:
+        pagina = pagina_44X.read()
+        print(pagina)
+    return
+
+
+def pagina_45():
+    print(f'Texto da página 45')
+    with open(r'txt\pagina 45.txt', 'r', encoding='utf-8') as pagina_45X:
+        pagina = pagina_45X.read()
+        print(pagina)
+    return
+
+
+def pagina_46():
+    print(f'Texto da página 46')
+    with open(r'txt\pagina 46.txt', 'r', encoding='utf-8') as pagina_46X:
+        pagina = pagina_46X.read()
+        print(pagina)
+    return
+
+
+def pagina_48():
+    print(f'Texto da página 48')
+    with open(r'txt\pagina 48.txt', 'r', encoding='utf-8') as pagina_48X:
+        pagina = pagina_48X.read()
+        print(pagina)
+    return
+
+
+def pagina_49():
+    print(f'Texto da página 49')
+    with open(r'txt\pagina 49.txt', 'r', encoding='utf-8') as pagina_49X:
+        pagina = pagina_49X.read()
+        print(pagina)
+    return
+
+
+def pagina_50():
+    print(f'Texto da página 50')
+    with open(r'txt\pagina 50.txt', 'r', encoding='utf-8') as pagina_50X:
+        pagina = pagina_50X.read()
+        print(pagina)
+    return
+
+
+def pagina_52():
+    print(f'Texto da página 52')
+    with open(r'txt\pagina 52.txt', 'r', encoding='utf-8') as pagina_52X:
+        pagina = pagina_52X.read()
+        print(pagina)
+    return
+
+
+def pagina_53():
+    print(f'Texto da página 53')
+    with open(r'txt\pagina 53.txt', 'r', encoding='utf-8') as pagina_53X:
+        pagina = pagina_53X.read()
+        print(pagina)
+    return
+
+
+def pagina_54():
+    print(f'Texto da página 54')
+    with open(r'txt\pagina 54.txt', 'r', encoding='utf-8') as pagina_54X:
+        pagina = pagina_54X.read()
+        print(pagina)
+    return
+
+
+def pagina_56():
+    print(f'Texto da página 56')
+    with open(r'txt\pagina 56.txt', 'r', encoding='utf-8') as pagina_56X:
+        pagina = pagina_56X.read()
+        print(pagina)
+    return
+
+
+def pagina_57():
+    print(f'Texto da página 57')
+    with open(r'txt\pagina 57.txt', 'r', encoding='utf-8') as pagina_57X:
+        pagina = pagina_57X.read()
+        print(pagina)
+    return
+
+
+
+
 """
-pagina_01()
 pagina_02()
 pagina_03()
 pagina_04()
@@ -227,4 +425,28 @@ pagina_27()
 pagina_28()
 pagina_29()
 pagina_30()
+pagina_31()
+pagina_32()
+pagina_33()
+pagina_34()
+pagina_35()
+pagina_36()
+pagina_37()
+pagina_38()
+pagina_39()
+pagina_40()
+pagina_41()
+pagina_42()
+pagina_43()
+pagina_44()
+pagina_45()
+pagina_46()
+pagina_48()
+pagina_49()
+pagina_50()
+pagina_52()
+pagina_53()
+pagina_54()
+pagina_56()
+pagina_57()
 """
