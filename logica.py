46f27d4e81703b998e7cc6b8696eda88248fe8ee
Felippe <frafaelrls@gmail.com>
2022-02-14 17:17:52 -0300

    adding logic to clear the terminal

--- logica.py
+++ logica.py
@@ -3,5 +3,13 @@
 - Deverá conter as chamadas das funções das páginas;
 - Fornecer uma resposta para o usuário conforme suas interações;
 """
-import paginas
+import os
 
+
+# Função para limpar o terminal
+def apagar():
+    limpar = "clear"  # Se for mac ou linux
+    if os.name in ("nt", "dos"):  # Se for windows
+        os.system('cls')
+    else:
+        os.system(limpar)
