3b55e0fffb33d8c6a5ffe1f735b3f853ba31b83e
Felippe <frafaelrls@gmail.com>
2022-02-16 17:28:03 -0300

    removing circular import error

--- paginas.py
+++ paginas.py
@@ -1,9 +1,9 @@
 """Páginas do livro"""
 import time
-import logica
 
 
 def pagina_01():
+    import logica
     logica.apagar()
     print(f"Texto da página 01")
     with open(r'txt\pagina 01.txt', 'r', encoding='utf-8') as pagina_01X:
@@ -396,8 +396,6 @@ def pagina_57():
         pagina = pagina_57X.read()
         print(pagina)
     return
-
-
 
 
 """
