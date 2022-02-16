3e24f0cb247bb8f2ec5ea06a589deb922159fa1c
Felippe <frafaelrls@gmail.com>
2022-02-16 18:05:36 -0300

    creating the logic of the first pages

--- logica.py
+++ logica.py
@@ -4,12 +4,39 @@
 - Fornecer uma resposta para o usuário conforme suas interações;
 """
 import os
+import paginas
 
 
 # Função para limpar o terminal
 def apagar():
-    limpar = "clear"  # Se for mac ou linux
-    if os.name in ("nt", "dos"):  # Se for windows
+    # Se mac ou linux
+    limpar = "clear"
+    # Se Windows
+    if os.name in ("nt", "dos"):
         os.system('cls')
     else:
         os.system(limpar)
+
+
+def leitura():
+    paginas.pagina_01()
+    os.system('pause')
+    apagar()
+    paginas.pagina_02()
+    os.system('pause')
+    apagar()
+    paginas.pagina_03()
+    os.system('pause')
+    apagar()
+    escolha = input(f"Se você deseja ir atrás do guarda escreva: seguir\n\n"
+                    f"Se você deseja espiar atrás da cortina escreva: "
+                    f"espiar\n")
+    escolha = escolha.lower()
+    if escolha == 'seguir':
+        apagar()
+        paginas.pagina_05()
+    else:
+        apagar()
+        paginas.pagina_19()
+
+
