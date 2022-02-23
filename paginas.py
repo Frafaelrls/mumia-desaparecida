ea56a4c260af4b1102cfd6e4b8ceebd2112c0915
Felippe <frafaelrls@gmail.com>
2022-02-23 14:02:02 -0300

    creating a single logic for reading all pages

--- paginas.py
+++ paginas.py
@@ -3,14 +3,16 @@ import os
 import time
 
 
-def pagina_01():
+def paginas(numero):
     import logica
     logica.apagar()
-    print(f"Texto da página 01")
-    with open(r'txt\pagina 01.txt', 'r', encoding='utf-8') as pagina_01X:
-        pagina = pagina_01X.read()
-        for palavra in pagina:
+    print(f"Texto da página {numero}")
+    numero = str(numero)
+    with open(r'txt\pagina ' + numero + '.txt', 'r', encoding='utf-8') as \
+            pagina:
+        pagina = pagina.read()
+        for letra in pagina:
             time.sleep(0.0)  # todo tempo zerado para teste
-            print(palavra, end='', flush=True)
+            print(letra, end='', flush=True)
     os.system('pause')
     return
