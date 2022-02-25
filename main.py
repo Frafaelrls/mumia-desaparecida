3756fef546cdd15f38c25ddee99215f4ef5cc41e
Felippe <frafaelrls@gmail.com>
2022-02-25 18:00:16 -0300

    adding answer validation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 import os
 import logica
+from logica import Cores
 
 
-titulo = "O mistério da Múmia Desaparecida\n"
+titulo = Cores.negrito + 'O mistério da Múmia Desaparecida\n\n' + Cores.reset
 introducao = """
 Você está prestes a iniciar a leitura de um livro interativo.
 Saiba que suas escolhas trarão consequências.
@@ -19,15 +20,25 @@ for letra in introducao:
     time.sleep(0.0)  # todo tempo zerado para teste
     print(letra, end='', flush=True)
 
+while True:
+    try:
+        resposta = input()
+        resposta = resposta.lower()
+        if resposta == "sim":
+            print('Que as aventuras comecem.\n')
+            time.sleep(0.0)  # todo tempo zerado para teste
+            os.system('pause')
+            logica.leitura()
+        elif resposta == 'nao' or 'não':
+            print('Volte quando tiver mais XP\n')
+            print('Fechando o livro....\nAperte qualquer tecla para finalizar')
+            os.system('pause >nul')
+            break
 
-resposta = input()
-resposta = resposta.lower()
-if resposta == "sim":
-    print("Que as aventuras comecem.\n")
-    time.sleep(0.0)  # todo tempo zerado para teste
-    os.system("pause")
-    logica.leitura()
-else:
-    print("Volte quando tiver mais XP\n")
-    print("FECHA O LIVRO")
-    os.system("pause")
+        else:
+            print('Você está em dúvida? digite sim ou não para iniciarmos')
+            os.system('pause')
+            continue
+    except ValueError:
+        print('Erro')
+        break
